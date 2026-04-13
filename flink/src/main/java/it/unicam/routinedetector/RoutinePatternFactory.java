6b3a4087892600e3876a2ef4207da1f6c326e7fb
lilliban <agranotti@gmail.com>
2026-04-13 13:39:19 +0200

    fix strategie di rilevamento routine

--- flink/src/main/java/it/unicam/routinedetector/RoutinePatternFactory.java
+++ flink/src/main/java/it/unicam/routinedetector/RoutinePatternFactory.java
@@ -16,116 +16,149 @@ public class RoutinePatternFactory {
             float percent,
             Duration tMax) {
         return switch (strategy) {
+            
             //compaiono tutti gli eventi della community
-            case A -> Pattern.<EnrichedEvent>begin("start",skipStrategy)
-                    .where(new SimpleCondition<EnrichedEvent>() {
-                        @Override
-                        public boolean filter(EnrichedEvent e){
-                            return communityName.equals(e.community);
-                        }
-                    })
-                    .times(activities.size())
-                    .within(tMax);
-            case B -> { //attualmente non ha finestre temporali attive
-                //compaiono tutti gli eventi della community
+            case A -> {
                 Pattern<EnrichedEvent, EnrichedEvent> pattern =
                         Pattern.<EnrichedEvent>begin("step_0", skipStrategy)
                                 .where(new SimpleCondition<EnrichedEvent>() {
                                     @Override
                                     public boolean filter(EnrichedEvent e) {
-                                        return e.activity.equals(activities.get(0));
+                                        return e.community.equals(communityName) && e.activity.equals(activities.get(0));
                                     }
                                 });
-
                 for (int i = 1; i < activities.size(); i++) {
                     final int idx = i;
-                    pattern = pattern.followedBy("step_" + idx)
+                    pattern = pattern.followedByAny("step_" + idx)
                             .where(new SimpleCondition<EnrichedEvent>() {
                                 @Override
                                 public boolean filter(EnrichedEvent e) {
-                                    return e.activity.equals(activities.get(idx));
+                                    return e.community.equals(communityName) && e.activity.equals(activities.get(idx));
                                 }
                             });
                 }
                 yield pattern;
             }
 
-            //scelto la strategia B..
+            //compaiono tutti gli eventi della community nell'ordine riportati nel pattern della community
+            case B -> {
+                Pattern<EnrichedEvent, EnrichedEvent> pattern =
+                        Pattern.<EnrichedEvent>begin("step_0", skipStrategy)
+                                .where(new SimpleCondition<EnrichedEvent>() {
+                                    @Override
+                                    public boolean filter(EnrichedEvent e) {
+                                        return e.community.equals(communityName) && e.activity.equals(activities.get(0));
+                                    }
+                                });
+                for (int i = 1; i < activities.size(); i++) {
+                    final int idx = i;
+                    pattern = pattern.followedBy("step_" + idx)
+                            .where(new SimpleCondition<EnrichedEvent>() {
+                                @Override
+                                public boolean filter(EnrichedEvent e) {
+                                    return e.community.equals(communityName) && e.activity.equals(activities.get(idx));
+                                }
+                            });
+                }
+                yield pattern;
+            }
+
             //condizioni A o B e permettiamo eventi di altre community nel mezzo
             case C -> Pattern.<EnrichedEvent>begin("start", skipStrategy)
                     .where(new SimpleCondition<EnrichedEvent>() {
                         @Override
-                        public boolean filter(EnrichedEvent e){
+                        public boolean filter(EnrichedEvent e) {
                             return communityName.equals(e.community);
                         }
                     })
                     .times(activities.size())
-                    .allowCombinations() //approssimazione perchè se volessimo precisione dovremmo codificare ogni evento con followedByAny
-                    .within(tMax);
+                    .allowCombinations();
 
             //condizioni A o B e NON permettiamo eventi di altre community nel mezzo
             case D -> Pattern.<EnrichedEvent>begin("start", skipStrategy)
                     .where(new SimpleCondition<EnrichedEvent>() {
                         @Override
-                        public boolean filter(EnrichedEvent e){
+                        public boolean filter(EnrichedEvent e) {
                             return communityName.equals(e.community);
                         }
                     })
                     .times(activities.size())
-                    .consecutive()
-                    .within(tMax);
-            //compaiono un numero di eventi >= ad una % di eventi della community
-            //come E considerando strategia A
-            case E -> {
-                int number = (int) (activities.size() * percent);
-                yield Pattern.<EnrichedEvent>begin("start", skipStrategy)
-                        .where(new SimpleCondition<EnrichedEvent>() {
-                            @Override
-                            public boolean filter(EnrichedEvent e){
-                                return communityName.equals(e.community);
-                            }
-                        })
-                        .times(number)
-                        .within(tMax);
-            }
-            case F -> {
-                int number = (int) (activities.size() * percent);
-                yield Pattern.<EnrichedEvent>begin("start",skipStrategy)
-                        .where(new SimpleCondition<EnrichedEvent>() {
-                            @Override
-                            public boolean filter(EnrichedEvent e){
-                                return communityName.equals(e.community);
-                            }
-                        })
-                        .times(number)
-                        .within(tMax);
-            }
-            //come E considerando strategia B
-            case G -> {
-                int number = (int) (activities.size() * percent);
-                yield Pattern.<EnrichedEvent>begin("start", skipStrategy)
-                        .where(new SimpleCondition<EnrichedEvent>() {
-                            @Override
-                            public boolean filter(EnrichedEvent e){
-                                return communityName.equals(e.community);
-                            }
-                        })
-                        .times(number)
-                        .consecutive()
-                        .within(tMax);
+                    .consecutive();
 
+            //compaiono un numero di eventi >= ad una % degli eventi della community (nessun vincolo di ordine)
+            case E -> {
+                int number = (int) Math.ceil(activities.size() * percent);
+                yield Pattern.<EnrichedEvent>begin("start", skipStrategy)
+                        .where(new SimpleCondition<EnrichedEvent>() {
+                            @Override
+                            public boolean filter(EnrichedEvent e) {
+                                return communityName.equals(e.community);
+                            }
+                        })
+                        .timesOrMore(number);
             }
-            //è passato un tempo T_max tra due eventi della community che danno origine a due attività distinte
-            case H -> Pattern.<EnrichedEvent>begin("start", skipStrategy)
+
+            //come E considerando strategia A (qualsiasi ordine, eventi di altre community ammessi)
+            case F -> {
+                int number = (int) Math.ceil(activities.size() * percent);
+                yield Pattern.<EnrichedEvent>begin("start", skipStrategy)
+                        .where(new SimpleCondition<EnrichedEvent>() {
+                            @Override
+                            public boolean filter(EnrichedEvent e) {
+                                return communityName.equals(e.community);
+                            }
+                        })
+                        .timesOrMore(number)
+                        .allowCombinations();
+            }
+
+            //come E considerando strategia B (ordine specifico del pattern)
+            case G -> {
+                int number = (int) Math.ceil(activities.size() * percent);
+                Pattern<EnrichedEvent, EnrichedEvent> pattern =
+                        Pattern.<EnrichedEvent>begin("step_0", skipStrategy)
+                                .where(new SimpleCondition<EnrichedEvent>() {
+                                    @Override
+                                    public boolean filter(EnrichedEvent e) {
+                                        return e.community.equals(communityName) && e.activity.equals(activities.get(0));
+                                    }
+                                });
+                for (int i = 1; i < number; i++) {
+                    final int idx = i;
+                    pattern = pattern.followedBy("step_" + idx)
+                            .where(new SimpleCondition<EnrichedEvent>() {
+                                @Override
+                                public boolean filter(EnrichedEvent e) {
+                                    return e.community.equals(communityName) && e.activity.equals(activities.get(idx));
+                                }
+                            });
+                }
+                yield pattern;
+            }
+
+            //e' passato un tempo >= T_max tra due eventi della community (gap = due attivita' distinte)
+            case H -> Pattern.<EnrichedEvent>begin("before_gap", skipStrategy)
                     .where(new SimpleCondition<EnrichedEvent>() {
                         @Override
-                        public boolean filter(EnrichedEvent e){
+                        public boolean filter(EnrichedEvent e) {
                             return communityName.equals(e.community);
                         }
                     })
-                    .oneOrMore()
-                    .consecutive()
-                    .within(tMax);
+                    .notFollowedBy("during_gap")
+                    .where(new SimpleCondition<EnrichedEvent>() {
+                        @Override
+                        public boolean filter(EnrichedEvent e) {
+                            return communityName.equals(e.community);
+                        }
+                    })
+                    .within(tMax)
+                    .followedBy("after_gap")
+                    .where(new SimpleCondition<EnrichedEvent>() {
+                        @Override
+                        public boolean filter(EnrichedEvent e) {
+                            return communityName.equals(e.community);
+                        }
+                    });
         };
 
     }
